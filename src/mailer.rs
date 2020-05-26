5f7d83b5bfc84e9d9172a192f61966c43e51414d
t-yng <t.yng.greenmonkey@gmail.com>
2020-05-26 23:24:59 +0900

    ベタ書きでPDFファイルを添付してメール送信する処理を実装

--- src/mailer.rs
+++ src/mailer.rs
@@ -1,17 +1,45 @@
+use std::path::Path;
+use std::fs;
 use lettre::{Message, SmtpTransport, Transport};
+use lettre::message::{SinglePart, header };
 use lettre::transport::smtp::{authentication::Credentials};
 
 // TODO: 送信先のKindel端末のメールアドレスを指定する
 // TODO: ユーザーのメールアドレス（設定ファイルから取得）
 // TODO: ユーザーのパスワード（設定ファイルから取得）
-pub fn sendmail(to_address: &str, user_gmail_address: &str, password: &str) -> Result<(), String> {
+pub fn sendmail(to_address: &str, user_gmail_address: &str, password: &str, file: &str) -> Result<(), String> {
     let smtp_server = "smtp.googlemail.com";
 
+    // REFACTOR: PDFの処理はモジュールを分けて引数で受け取るようにする
+    // pdfファイルを読み込み
+    let path = Path::new(file);
+    let file_name = match path.file_name() {
+        Some(file_name) => file_name.to_str().unwrap(),
+        None => return Err(format!("Could not get filename: {}", file)),
+    };
+    let pdf = fs::read(file).unwrap();
+
+    // TODO: pdfファイルが複数の場合を考慮する
+    // lettreの実装example
+    // @see: https://github.com/lettre/lettre/blob/master/src/message/mod.rs
     let email = Message::builder()
         .to(to_address.parse().unwrap())
         .from(user_gmail_address.parse().unwrap())
         .subject("send some files to my kindle by kindle-push")
-        .body("")
+        .singlepart(
+            SinglePart::base64()
+            .header(header::ContentType("application/pdf".parse().unwrap()))
+            .header(header::ContentDisposition {
+                disposition: header::DispositionType::Attachment,
+                parameters: vec![
+                    header::DispositionParam::Filename(
+                        header::Charset::Ext("utf-8".into()),
+                        None, file_name.as_bytes().into()
+                    )
+                ]
+            })
+            .body(pdf)
+        )
         .unwrap();
 
     let credentials = Credentials::new(
@@ -35,16 +63,24 @@ pub fn sendmail(to_address: &str, user_gmail_address: &str, password: &str) -> R
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
     #[test]
     #[ignore]
-    fn sendmail() {
+    fn test_sendmail() {
         // TODO: テストを書く
         // メール送信の良いテストの書き方が思いつかないので一旦保留にする（言い訳）
         // テスト記述の参考情報
+        // ローカルでSMTPサーバー起動する？
         // @see: https://github.com/lettre/lettre/tree/master/tests
         // @see: https://medium.com/@11Takanori/simple-mocking-in-rust-3a21f1fa7e0c
+        let to_address = "<to_addres>";
+        let user_gmail_address = "<user_gmail_address>";
+        let password = "<google_application_password>";
+        let file = "<pdf_file_path>";
+
+        let result = sendmail(to_address, user_gmail_address, password, file);
+        assert_eq!(result, Ok(()));
     }
 
 }
