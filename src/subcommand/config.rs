8813d25ee8130b87a69c840b37ad07331c8e1a7d
t-yng <t.yng.greenmonkey@gmail.com>
2020-06-06 18:05:18 +0900

    kindle_nameの値にデフォルト値を追加

--- src/subcommand/config.rs
+++ src/subcommand/config.rs
@@ -2,10 +2,16 @@ use std::io::{self, Write};
 use crate::config::{Credentials, Kindle, Config};
 
 pub fn config() -> io::Result<()> {
+    let kindle_name_default = "default";
+
     // TODO: バリデーションを追加
+    // TODO: 既に設定ファイルが存在する時は更新の挙動にする
     let user_gmail_address = get_user_input("user_gmail_address: ")?;
     let google_application_password = get_user_input("google_application_password: ")?;
-    let kindle_name = get_user_input("kindle_name: ")?;
+    let kindle_name = get_user_input_default(
+        format!("kindle_name [{}]: ", kindle_name_default).as_str(),
+        "default"
+    )?;
     let kindle_mail_address = get_user_input("kindle_mail_address: ")?;
 
     let kindle = Kindle {
@@ -19,6 +25,15 @@ pub fn config() -> io::Result<()> {
     };
 
     Config::save(vec![kindle], credentials)
+}
+
+fn get_user_input_default(prompt: &str, default: &str) -> Result<String, io::Error> {
+    let input = get_user_input(prompt)?;
+
+    match input.as_str() {
+        "" => Ok(default.to_string()),
+        input => Ok(input.to_string())
+    }
 }
 
 fn get_user_input(prompt: &str) -> Result<String, io::Error> {
